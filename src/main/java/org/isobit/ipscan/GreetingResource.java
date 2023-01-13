8f2328dd76d4a5ca5fabd17e7f319ca242e38dfa
Erik Antonio Alarcon Pinedo <ealarconp@regionancash.gob.pe>
2023-01-13 15:17:28 -0500

    disable test

--- src/main/java/org/isobit/ipscan/GreetingResource.java
+++ src/main/java/org/isobit/ipscan/GreetingResource.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
